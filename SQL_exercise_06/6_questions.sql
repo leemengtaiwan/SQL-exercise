79bd2b656483b39f848d6f2e495ab6f402bdfe4a
leemengtaiwan <b98705001@gmail.com>
2018-02-24 21:24:19 +0900

    Complete exercise 6, 7

--- SQL_exercise_06/6_questions.sql
+++ SQL_exercise_06/6_questions.sql
@@ -2,4 +2,17 @@
 -- 6.1 List all the scientists' names, their projects' names, 
     -- and the hours worked by that scientist on each project, 
     -- in alphabetical order of project name, then scientist name.
+SELECT S.Name AS Scientist, P.Name AS Project, P.Hours
+  FROM Scientists S
+    INNER JOIN AssignedTo A ON S.SSN = A.Scientist
+    INNER JOIN Projects P ON P.Code = A.Project
+  ORDER BY P.Name, S.Name;
+
 -- 6.2 Select the project names which are not assigned yet
+SELECT Name
+  FROM Projects
+  WHERE Code NOT IN
+  (
+    SELECT DISTINCT(Project)
+      FROM AssignedTo
+  );
--- SQL_exercise_07/7_questions.sql
+++ SQL_exercise_07/7_questions.sql
@@ -1,4 +1,72 @@
 -- https://en.wikibooks.org/wiki/SQL_Exercises/Planet_Express 
 -- 7.1 Who receieved a 1.5kg package?
     -- The result is "Al Gore's Head".
+-- Subquery ver
+SELECT Name
+  FROM Client
+  WHERE AccountNumber IN
+  (
+    SELECT Recipient
+    FROM Package
+    WHERE Weight = 1.5
+  );
+
+-- JOIN ver
+SELECT Client.Name
+  FROM Client INNER JOIN Package
+  ON Client.AccountNumber = Package.Recipient
+  WHERE Package.Weight = 1.5;
+
 -- 7.2 What is the total weight of all the packages that he sent?
+-- JOIN ver
+SELECT SUM(P.Weight)
+  FROM Package P INNER JOIN Client C
+  ON P.Sender = C.AccountNumber
+  WHERE C.Name = "Al Gore's Head";
+
+-- Subquery ver
+SELECT SUM(Weight)
+  FROM Package
+  WHERE Sender IN
+  (
+    SELECT AccountNumber
+    FROM Client
+    WHERE Name = "Al Gore's Head"
+  );
+
+-- 7.3 Which pilots transported those packages?
+-- Subquery ver
+SELECT Name
+  FROM Employee
+  WHERE EmployeeID IN
+  (
+    SELECT Manager
+    FROM Shipment
+    WHERE ShipmentID IN
+    (
+      SELECT Shipment
+      FROM Package
+      WHERE Sender IN
+      (
+        SELECT AccountNumber
+        FROM Client
+        WHERE Name = "Al Gore's Head"
+      )
+    )
+  );
+
+-- JOIN ver
+SELECT E.Name
+  FROM Employee E INNER JOIN Shipment S
+  ON E.EmployeeID = S.Manager
+  WHERE ShipmentID IN
+  (
+    SELECT Shipment
+    FROM Package
+    WHERE Sender IN
+    (
+      SELECT AccountNumber
+      FROM Client
+      WHERE Name = "Al Gore's Head"
+    )
+  );
